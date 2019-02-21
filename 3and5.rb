bf6aa8f10d900dda1870b5a4505298b51723a634
prestontewell <preston@tewellphoto.com>
2019-02-21 12:07:11 -0600

    add problem exercise 3and5

---
+++ 3and5.rb
@@ -0,0 +1,27 @@
+
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+# list the numbers from one to ten
+
+# list = 0
+# 10.times do 
+#   list += 1
+#   p list
+# end
+
+# now find which of those numbers are multiples of 3 and 5
+# let's start with multiples of 3
+value = []
+
+list = 0
+10.times do
+  list +=1
+  if list % 3 == 0
+    p list
+    else
+  end
+end
